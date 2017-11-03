e49ee81b1e50678393d4306e119438ea6de7c9fb
Michael Kemp <mkemp@accu-time.com>
2017-11-03 15:29:53 -0400

    Basic Camera

--- core/src/com/mygdx/cameras/Cameras.java
+++ core/src/com/mygdx/cameras/Cameras.java
@@ -3,23 +3,36 @@ package com.mygdx.cameras;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Cameras extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	OrthographicCamera camera;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("image.png");
+		camera = new OrthographicCamera(
+				Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight()
+		);
+		camera.translate(
+				camera.viewportWidth / 2,
+				camera.viewportHeight / 2
+		);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
