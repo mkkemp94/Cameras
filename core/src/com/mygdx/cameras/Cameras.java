c15ed16131a36efd7deb010f3b1f73ecefa6d36e
Michael Kemp <mkemp@accu-time.com>
2017-11-03 15:40:16 -0400

    Pixel-independent camera

--- core/src/com/mygdx/cameras/Cameras.java
+++ core/src/com/mygdx/cameras/Cameras.java
@@ -2,28 +2,40 @@ package com.mygdx.cameras;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Cameras extends ApplicationAdapter {
+public class Cameras extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	Texture img;
+	Sprite theWorld;
 	OrthographicCamera camera;
+
+	final float WORLD_WIDTH = 50;
+	final float WORLD_HEIGHT = 25;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("image.png");
-		camera = new OrthographicCamera(
-				Gdx.graphics.getWidth(),
-				Gdx.graphics.getHeight()
+		theWorld = new Sprite(new Texture("image.png"));
+		theWorld.setPosition(0, 0);
+		theWorld.setSize(50, 25);
+
+		float aspectRatio =
+				(float) Gdx.graphics.getHeight() /
+				(float) Gdx.graphics.getWidth();
+
+		camera = new OrthographicCamera(25 * aspectRatio, 25);
+		camera.position.set(
+				WORLD_WIDTH / 2,
+				WORLD_HEIGHT / 2, 0
 		);
-		camera.translate(
-				camera.viewportWidth / 2,
-				camera.viewportHeight / 2
-		);
+
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -34,13 +46,61 @@ public class Cameras extends ApplicationAdapter {
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		theWorld.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(1f,0f);
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-1f,0f);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0f,1f);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0f,-1f);
+
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
